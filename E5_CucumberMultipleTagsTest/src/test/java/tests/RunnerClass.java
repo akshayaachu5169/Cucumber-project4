28428547d4046074b90f808e67f0ec93f025252d
akshayaachu5169 <akshayaachu5169@gmail.com>
2023-06-19 12:52:44 +0530

    initial commit

---
+++ E5_CucumberMultipleTagsTest/src/test/java/tests/RunnerClass.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(tags= "not @validuser")
+public class RunnerClass {
+
+	}
